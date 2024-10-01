10ae72b713794b31341fa29da1700587e1ac9bdf
Danny <danny.f.gomez@gmail.com>
2024-10-01 17:11:25 -0400

    create controller routes for finding user by id and updating user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     console.log(body);
     this.usersService.create(body.email, body.password);
   }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+ @Patch('/:id')
+ updateUser(@Param('id') id:string, @Body() body: CreateUserDto){
+    return this.usersService.update(parseInt(id), body)
+ }
+
+
 }
