bc09710a24b740241559b17772d9b68785a30665
Danny <danny.f.gomez@gmail.com>
2024-10-01 17:01:24 -0400

    create remove method in users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -31,5 +31,12 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id)
+    if (!user) {
+        throw new Error('User not found')
+    }
+
+    return this.repo.remove(user)
+  }
 }
