bf2a269736b8730cc8b2b57a5b8cd72be40a9b37
Danny <danny.f.gomez@gmail.com>
2024-10-02 11:31:10 -0400

    import use interceptors into users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,12 +7,13 @@ import {
   Param,
   Query,
   Delete,
-  NotFoundException
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Not } from 'typeorm';
 
 @Controller('auth')
 export class UsersController {
@@ -24,6 +25,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
