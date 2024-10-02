c33436152188e33dee7f338c3c0c2aef59648356
Danny <danny.f.gomez@gmail.com>
2024-10-02 11:21:58 -0400

    add exceptions if user is not found

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,13 @@ import {
   Patch,
   Param,
   Query,
-  Delete
+  Delete,
+  NotFoundException
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Not } from 'typeorm';
 
 @Controller('auth')
 export class UsersController {
@@ -23,8 +25,12 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException("user not found");
+    }
+    return user
   }
 
   @Get('')
