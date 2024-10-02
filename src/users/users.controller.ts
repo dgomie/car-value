77fa3785730d50469a0f9c8e0ef682a0bcd488e4
Danny <danny.f.gomez@gmail.com>
2024-10-02 12:17:11 -0400

    apply interceptor to entire user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,6 +17,7 @@ import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -27,7 +28,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @Serialize(UserDto)
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
