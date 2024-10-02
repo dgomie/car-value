b7871e5a4729af917b77fb50e2a5ef44294d2587
Danny <danny.f.gomez@gmail.com>
2024-10-02 12:01:04 -0400

    import user.dto into custom interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,20 +7,39 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
+
 
 export class SerializeInterceptor implements NestInterceptor {
-  intercept(
-    context: ExecutionContext,
-    handler: CallHandler<any>,
-  ): Observable<any> {
-    //Run something before a request is handled by the request handler
-    console.log('Im running before the handler', context);
-    
-    return handler.handle().pipe(
-      map((data: any) => {
-        //run something before the response is sent out
-        console.log('Im running before the response is sent out', data);
-      }),
-    );
+    intercept(
+      context: ExecutionContext,
+      handler: CallHandler<any>,
+    ): Observable<any> {
+      
+  
+      return handler.handle().pipe(
+        map((data: any) => {
+          return plainToClass(UserDto, data, {
+            excludeExtraneousValues: true,
+          })
+        }),
+      );
+    }
   }
-}
+
+// export class SerializeInterceptor implements NestInterceptor {
+//   intercept(
+//     context: ExecutionContext,
+//     handler: CallHandler<any>,
+//   ): Observable<any> {
+//     //Run something before a request is handled by the request handler
+//     console.log('Im running before the handler', context);
+
+//     return handler.handle().pipe(
+//       map((data: any) => {
+//         //run something before the response is sent out
+//         console.log('Im running before the response is sent out', data);
+//       }),
+//     );
+//   }
+// }
